45f538e1278e08c1da3f3444ff44a4e47ec48af7
Yossi Segev <yossisegev.dev@gmail.com>
2017-11-06 22:46:21 +0200

    init

---
+++ app/src/main/java/com/yossisegev/cityguide/MainActivity.java
@@ -0,0 +1,28 @@
+package com.yossisegev.cityguide;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button learnMore = findViewById(R.id.activity_main_learn_more);
+        learnMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://en.wikipedia.org/wiki/London"));
+                startActivity(browserIntent);
+            }
+        });
+    }
+}
